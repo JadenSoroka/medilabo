b9f38682ed517fa2d5d4b9d0398d6a7a2f470dec
Jaden Soroka <jaden.c.soroka@sherwin.com>
2026-03-09 10:58:49 -0400

    Initialized physiciannotes microservice

---
+++ physiciannotes/src/main/java/com/medilabo/physiciannotes/PhysiciannotesApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.physiciannotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiciannotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiciannotesApplication.class, args);
+	}
+
+}
---
+++ physiciannotes/src/test/java/com/medilabo/physiciannotes/PhysiciannotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.physiciannotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiciannotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
