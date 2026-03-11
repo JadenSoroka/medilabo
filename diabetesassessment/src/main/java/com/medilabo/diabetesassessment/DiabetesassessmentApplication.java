e7780a48e449c56e47272f6347cdd46d792e5fa9
Jaden Soroka <jaden.c.soroka@sherwin.com>
2026-03-11 09:43:10 -0400

    diabetes assessment service initialization

---
+++ diabetesassessment/src/main/java/com/medilabo/diabetesassessment/DiabetesassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.diabetesassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesassessmentApplication.class, args);
+	}
+
+}
---
+++ diabetesassessment/src/test/java/com/medilabo/diabetesassessment/DiabetesassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.diabetesassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
